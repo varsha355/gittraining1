d03a6e53d386cf7a463cf8fedd5b5dae4a8da434
varsha <varsha@gmail.com>
2024-03-22 10:51:31 +0530

    intial commit

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Prime numbers up to %d are: ", n);
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i))
+            printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
